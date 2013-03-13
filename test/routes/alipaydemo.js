3db670054632b4e43bcc4c80749f8458a928ee63
lodengo <ddl5200@gmail.com>
2013-03-13 10:56:39 +0800

    Update alipaydemo.js

--- test/routes/alipaydemo.js
+++ test/routes/alipaydemo.js
@@ -1,5 +1,20 @@
-﻿var alipay = require('../alipay_config').alipay;
-//alipay.on('verify_fail', function(){console.log('emit verify_fail')});	
+var alipay = require('../alipay_config').alipay;
+
+alipay.on('verify_fail', function(){console.log('emit verify_fail')})
+	.on('create_direct_pay_by_user_trade_finished', function(out_trade_no, trade_no){})
+	.on('create_direct_pay_by_user_trade_success', function(out_trade_no, trade_no){})	
+	.on('refund_fastpay_by_platform_pwd_success', function(batch_no, success_num, result_details){})
+	.on('create_partner_trade_by_buyer_wait_buyer_pay', function(out_trade_no, trade_no){})
+	.on('create_partner_trade_by_buyer_wait_seller_send_goods', function(out_trade_no, trade_no){})
+	.on('create_partner_trade_by_buyer_wait_buyer_confirm_goods', function(out_trade_no, trade_no){})
+	.on('create_partner_trade_by_buyer_trade_finished', function(out_trade_no, trade_no){})
+	.on('send_goods_confirm_by_platform_fail', function(error){res.send(error)})
+	.on('send_goods_confirm_by_platform_success', function(out_trade_no, trade_no, xml){})
+	.on('trade_create_by_buyer_wait_buyer_pay', function(out_trade_no, trade_no){})
+	.on('trade_create_by_buyer_wait_seller_send_goods', function(out_trade_no, trade_no){})
+	.on('trade_create_by_buyer_wait_buyer_confirm_goods', function(out_trade_no, trade_no){})
+	.on('trade_create_by_buyer_trade_finished', function(out_trade_no, trade_no){});	
+	
 	
 exports.create_direct_pay_by_user = function(req, res){
 	if(req.route.method == 'get'){
@@ -14,10 +29,7 @@ exports.create_direct_pay_by_user = function(req, res){
 			,show_url:req.body.WIDshow_url
 		 };		
 		
-		alipay.create_direct_pay_by_user(data, res)
-		.on('verify_fail', function(){})
-		.on('create_direct_pay_by_user_trade_finished', function(out_trade_no, trade_no){})
-		.on('create_direct_pay_by_user_trade_success', function(out_trade_no, trade_no){});
+		alipay.create_direct_pay_by_user(data, res);		
 	}
 }
 
@@ -31,12 +43,9 @@ exports.refund_fastpay_by_platform_pwd =function(req, res){
 			,batch_no:req.body.WIDbatch_no 
 			,batch_num:req.body.WIDbatch_num 
 			,detail_data: req.body.WIDdetail_data			
-		 };
+		 };	
 		 	
-		alipay.refund_fastpay_by_platform_pwd(data, res)
-		.on('verify_fail', function(){})
-		.on('refund_fastpay_by_platform_pwd_success', function(batch_no, success_num, result_details){})
-		
+		alipay.refund_fastpay_by_platform_pwd(data, res);		
 	}
 }
 
@@ -45,20 +54,24 @@ exports.create_partner_trade_by_buyer = function(req, res){
 		res.render('create_partner_trade_by_buyer'); 
 	}
 	else if(req.route.method == 'post'){		
-		var data = {
-			 refund_date:req.body.WIDrefund_date 
-			,batch_no:req.body.WIDbatch_no 
-			,batch_num:req.body.WIDbatch_num 
-			,detail_data: req.body.WIDdetail_data			
-		 };	
+		 var data = {
+			out_trade_no	: req.body.WIDout_trade_no,
+			subject	: req.body.WIDsubject,
+			price	: req.body.WIDprice,
+			quantity	: req.body.WIDquantity,
+			logistics_fee	: req.body.WIDlogistics_fee,
+			logistics_type	: req.body.WIDlogistics_type,
+			logistics_payment	: req.body.WIDlogistics_payment,
+			body	: req.body.WIDbody,
+			show_url	: req.body.WIDshow_url,
+			receive_name	: req.body.WIDreceive_name,
+			receive_address	: req.body.WIDreceive_address,
+			receive_zip	: req.body.WIDreceive_zip,
+			receive_phone	: req.body.WIDreceive_phone,
+			receive_mobile	: req.body.WIDreceive_mobile		
+		 };
 		
-		alipay.refund_fastpay_by_platform_pwd(data, res)
-		.on('verify_fail', function(){})
-		.on('create_partner_trade_by_buyer_wait_buyer_pay', function(out_trade_no, trade_no){})
-		.on('create_partner_trade_by_buyer_wait_seller_send_goods', function(out_trade_no, trade_no){})
-		.on('create_partner_trade_by_buyer_wait_buyer_confirm_goods', function(out_trade_no, trade_no){})
-		.on('create_partner_trade_by_buyer_trade_finished', function(out_trade_no, trade_no){});
-		
+		alipay.create_partner_trade_by_buyer(data, res);		
 	}
 }
 
@@ -74,9 +87,7 @@ exports.send_goods_confirm_by_platform = function(req, res){
 			,transport_type: req.body.WIDtransport_type			
 		 };	
 		
-		alipay.send_goods_confirm_by_platform(data, res)
-		.on('send_goods_confirm_by_platform_fail', function(error){res.send(error)})
-		.on('send_goods_confirm_by_platform_success', function(out_trade_no, trade_no, xml){});		
+		alipay.send_goods_confirm_by_platform(data, res);			
 	}
 }
 
@@ -95,10 +106,7 @@ exports.create_direct_bankpay_by_user = function(req, res){
 			,defaultbank:req.body.WIDdefaultbank			
 		 };		
 		
-		alipay.create_direct_pay_by_user(data, res)
-		.on('verify_fail', function(){})
-		.on('create_direct_pay_by_user_trade_finished', function(out_trade_no, trade_no){})
-		.on('create_direct_pay_by_user_trade_success', function(out_trade_no, trade_no){});		
+		alipay.create_direct_pay_by_user(data, res);			
 	}
 }
 
@@ -108,20 +116,23 @@ exports.trade_create_by_buyer = function(req, res){
 	}
 	else if(req.route.method == 'post'){		
 		var data = {
-			 out_trade_no:req.body.WIDout_trade_no 
-			,subject:req.body.WIDsubject 
-			,total_fee:req.body.WIDtotal_fee 
-			,body: req.body.WIDbody
-			,show_url:req.body.WIDshow_url
-			,paymethod:'bankPay'
-			,defaultbank:req.body.WIDdefaultbank			
+			out_trade_no	: req.body.WIDout_trade_no,
+			subject	: req.body.WIDsubject,
+			price	: req.body.WIDprice,
+			quantity	: req.body.WIDquantity,
+			logistics_fee	: req.body.WIDlogistics_fee,
+			logistics_type	: req.body.WIDlogistics_type,
+			logistics_payment	: req.body.WIDlogistics_payment,
+			body	: req.body.WIDbody,
+			show_url	: req.body.WIDshow_url,
+			receive_name	: req.body.WIDreceive_name,
+			receive_address	: req.body.WIDreceive_address,
+			receive_zip	: req.body.WIDreceive_zip,
+			receive_phone	: req.body.WIDreceive_phone,
+			receive_mobile	: req.body.WIDreceive_mobile			
 		 };		
 		
-		alipay.trade_create_by_buyer(data, res)
-		.on('verify_fail', function(){})
-		.on('trade_create_by_buyer_wait_buyer_pay', function(out_trade_no, trade_no){})
-		.on('trade_create_by_buyer_wait_seller_send_goods', function(out_trade_no, trade_no){})
-		.on('trade_create_by_buyer_wait_buyer_confirm_goods', function(out_trade_no, trade_no){})
-		.on('trade_create_by_buyer_trade_finished', function(out_trade_no, trade_no){});	
+		alipay.trade_create_by_buyer(data, res);
+		
 	}
 }
